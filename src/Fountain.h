4877e5b69094dc49d8cac534d3739335ac349ede
kimgab <kimgab@cs.wallawalla.edu>
2020-02-24 21:10:23 +0000

    autosave

--- src/Fountain.h
+++ src/Fountain.h
@@ -1,7 +1,15 @@
 #ifndef FOUNTAIN_H
 #define FOUNTAIN_H
+
+#include "Review.h"
+#include <map>
+#include <string>
+using namespace std;
 class Fountain {
 public:
 private:
+  string location;
+  int x, y;
+  map<string, Review> *RevMap;
 };
 #endif // FOUNTAIN_H
